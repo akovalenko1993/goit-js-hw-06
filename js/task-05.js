a4ce6e6c6a18c6c152f76a3cb004a468948f6d9a
akovalenko1993 <alex.kovalenko.1993@gmail.com>
2022-03-14 21:31:29 +0200

    fff

---
+++ js/task-05.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+// input.addEventListener('input', (input,output) =>  output.textContent=input.textContent )
+console.dir(output);
